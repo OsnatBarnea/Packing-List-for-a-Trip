00c376b9cb207c11c953b2e748aa291752e73d8b
OsnatBarnea <osnatbarnea1@gmail.com>
2024-07-23 15:50:40 +0300

    fits mobile screen

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //get elements:
 const equipmentBox = document.getElementById("equipmentBox");
 const amountBox = document.getElementById("amountBox");
@@ -7,16 +9,16 @@ const container = document.getElementById("container");
 equipmentBox.focus();
 
 ////when user type's the background color is default (even if validation was applied)
-equipmentBox.addEventListener(`input`, function(){
-equipmentBox.style.backgroundColor = "";
+equipmentBox.addEventListener(`input`, function () {
+    equipmentBox.style.backgroundColor = "";
 });
 
-amountBox.addEventListener(`input`, function(){
-amountBox.style.backgroundColor = "";
+amountBox.addEventListener(`input`, function () {
+    amountBox.style.backgroundColor = "";
 });
 
-document.getElementById('subButton').addEventListener('click', function() {
-document.getElementById('purchaseBox').selectedIndex = 0;
+document.getElementById('subButton').addEventListener('click', function () {
+    document.getElementById('purchaseBox').selectedIndex = 0;
 });
 
 
@@ -24,26 +26,27 @@ document.getElementById('purchaseBox').selectedIndex = 0;
 let equipmentList = [];
 
 //load saved list from the local storage
-function loadList(){
+function loadList() {
     const json = localStorage.getItem("equipmentList");
 
-    if(!json) return;
+    if (!json) return;
 
     equipmentList = JSON.parse(json);
 
     addToTableList(equipmentList);
 }
 
-
 //add data from the user
-function submit(){
+function submit() {
 
     if (!validation()) return;
-    
 
-    const oneEquipment = {equipment: equipmentBox.value, 
-                            amount: amountBox.value, 
-                            purchase:purchaseBox.value};
+
+    const oneEquipment = {
+        equipment: equipmentBox.value,
+        amount: amountBox.value,
+        purchase: purchaseBox.value
+    };
 
     equipmentList.push(oneEquipment);
 
@@ -55,51 +58,51 @@ function submit(){
     saveList();
 }
 
-
-function clear(){
-    equipmentBox.value = amountBox.value = purchaseBox.value.innerHTML = "";
-    // purchase.checked = false; //clear
+function clear() {
+    equipmentBox.value = amountBox.value = "";
+    purchaseBox.value = "select";
     equipmentBox.focus();
 }
 
 //missing items:
-function validation(){
+function validation() {
     // radioValue();
-    if (!equipmentBox.value){
+    if (!equipmentBox.value) {
         alert("You did not enter the equipment name");
         equipmentBox.focus();
         equipmentBox.style.backgroundColor = "#FFD4C6";
         return false;
     }
-    if (!amountBox.value){
+    if (!amountBox.value) {
         alert("Please enter the amount needed");
         amountBox.focus();
         amountBox.value = "";
         amountBox.style.backgroundColor = "#FFD4C6";
         return false;
     }
-    if (amountBox.value <= 0){
+    if (amountBox.value <= 0) {
         alert("Please enter a positive number");
         amountBox.focus();
         amountBox.value = "";
         amountBox.style.backgroundColor = "#FFD4C6";
         return false;
     }
-    if(purchaseBox.value === "select an option..."){
+    if (purchaseBox.value === "select") {
         alert("Please choose an option - do you need to purchase the item?");
-                return false;
+        return false;
     }
     return true;
 }
 
 //present the list in table:
-function addToTableList(equipmentList){
+function addToTableList(equipmentList) {
 
-    if(equipmentList.length === 0) {
+    if (equipmentList.length === 0) {
         container.innerHTML = "";
-        return;}
+        return;
+    }
 
-   let html = `<table class="tableList">
+    let html = `<table class="tableList">
                     <thead>
                         <tr class="row">
                             <th>Equipment</th>
@@ -108,53 +111,50 @@ function addToTableList(equipmentList){
                             
                         </tr>    
                     </thead>        
-                <tbody>`; 
-    for (let i = 0; i < equipmentList.length; i++){
+                <tbody>`;
+    for (let i = 0; i < equipmentList.length; i++) {
         html += `<tr class="bodyRow">
                     <td contentEditable>${equipmentList[i].equipment}</td>
                     <td contentEditable>${equipmentList[i].amount}</td>
                     <td contentEditable>${equipmentList[i].purchase}</td>
                     <td><button onclick="deleteItem(${i})" class="deleteButton"><img src="assets/images/trash.svg" class="trashImg" alt="delete"></button></td>
                  </tr>`;
-    }            
+    }
     html += `</tbody></table>`;
 
     //add the list to the div
     container.innerHTML = html;
 
     // Hide delete buttons initially
-let deleteButtons = document.querySelectorAll('.deleteButton');
-deleteButtons.forEach(button => {
+    let deleteButtons = document.querySelectorAll('.deleteButton');
+    deleteButtons.forEach(button => {
         button.style.visibility = 'hidden';
     });
 
-        // Add hover effect to show delete button
-        let bodyRow = document.querySelectorAll('.bodyRow');
-        bodyRow.forEach(row => {
-            row.addEventListener('mouseenter', () => {
-                row.querySelector('.deleteButton').style.visibility = 'visible';
-            });
-            row.addEventListener('mouseleave', () => {
-                row.querySelector('.deleteButton').style.visibility = 'hidden';
-            });
+    // Add hover effect to show delete button
+    let bodyRow = document.querySelectorAll('.bodyRow');
+    bodyRow.forEach(row => {
+        row.addEventListener('mouseenter', () => {
+            row.querySelector('.deleteButton').style.visibility = 'visible';
         });
+        row.addEventListener('mouseleave', () => {
+            row.querySelector('.deleteButton').style.visibility = 'hidden';
+        });
+    });
 }
 
-function deleteItem(index){
-    const sure = confirm("are you sure you want to delete " + equipmentList[index].equipment +"?");
-    if(!sure) return;
+function deleteItem(index) {
+    const sure = confirm("are you sure you want to delete " + equipmentList[index].equipment + "?");
+    if (!sure) return;
 
-    equipmentList.splice(index, 1);  
+    equipmentList.splice(index, 1);
 
     addToTableList(equipmentList);
 
     saveList();
 }
 
-
-
-
-function saveList(){
+function saveList() {
     const json = JSON.stringify(equipmentList);
 
     localStorage.setItem("equipmentList", json);
